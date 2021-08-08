ded8693f9eb3dd43871e58fe3a3aef897ac32d9b
Karthik Amrutavakkula <akarthik1910@gmail.com>
2021-08-08 09:39:28 +0530

    Added Test cases

--- test_app.py
+++ test_app.py
@@ -17,10 +17,45 @@ def test_pred_virginica():
         "sepal_length": 3,
         "sepal_width": 5,
         "petal_length": 3.2,
-        "petal_width": 4.4,
+        "petal_width": 4.4
     }
     with TestClient(app) as client:
         response = client.post("/predict_flower", json=payload)
         # asserting the correct response is received
         assert response.status_code == 200
-        assert response.json() == {"flower_class": "Iris Virginica"}
+        assert response.json() ["flower_class"]=="Iris Virginica"
+        assert "datetime" in response.json()
+
+ 
+# test to check if Iris Virginica is classified correctly
+def test_new_testcase1():
+    # defining a sample payload for the testcase
+     payload = {
+        "sepal_length": 5.84,
+        "sepal_width": 3.05,
+        "petal_length": 3.76,
+        "petal_width": 1.20
+     }
+     with TestClient(app) as client:
+          response = client.post("/predict_flower", json=payload)
+         # asserting the correct response is received
+          assert response.status_code == 200
+          assert response.json() ["flower_class"]=="Iris Versicolour"
+          assert "datetime" in response.json()
+
+
+# test to check if Iris Virginica's petal dimensions were classified correctly       
+def test_new_testcase2():
+    # defining a sample payload for the testcase
+     payload = {
+        "sepal_length": 5.0,
+        "sepal_width": 3.0,
+        "petal_length": 1.6,
+        "petal_width": 0.2
+     }
+     with TestClient(app) as client:
+          response = client.post("/predict_flower", json=payload)
+         # asserting the correct response is received
+          assert response.status_code == 200
+          assert response.json() ["flower_class"]=="Iris Setosa"
+          assert "datetime" in response.json()
